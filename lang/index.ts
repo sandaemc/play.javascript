4922e12aa26d0ac14729c091360bd153f7f042b5
Sandae Macalalag <sandaemc@gmail.com>
2022-08-03 03:14:02 +0000

    Added templates

--- lang/index.ts
+++ lang/index.ts
@@ -1,4 +1,20 @@
+import _ from 'lodash';
 
 (async () => {
+    // sort the array of integers
+    const nums = [BigInt(16592875584896661), BigInt(16592875326306478), BigInt(16592875219101532)];
+
+    const sorted = _.sortBy(nums);
+
+    console.log({
+        nums,
+        sorted,
+        samplesort: _.sortBy([3, 2, 1])
+    });
+
+    if (BigInt(16592875584896661) > BigInt(16592875219101532)) {
+        console.log('bigger');
+    }
+
 
 })();
