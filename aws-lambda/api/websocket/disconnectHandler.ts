14d6e0e3cf5f0b73b2b16b8fd372b37b04b19b10
Sandae <gogosandae@gmail.com>
2022-08-25 10:27:22 +0800

    deleted cdk plays

--- aws-lambda/api/websocket/disconnectHandler.ts
+++
@@ -1,9 +0,0 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    console.info(JSON.stringify(event));
-    return {
-        statusCode: 200,
-        body: JSON.stringify(event)
-    }
-}
